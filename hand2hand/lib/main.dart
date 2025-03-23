0601a2e3153b655c0cd45396980bf15189f12f6c
matildensousa <up202305502@up.pt>
2025-03-23 15:35:26 +0000

    Supabase Import Added

--- hand2hand/lib/main.dart
+++ hand2hand/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 const supabaseUrl = 'https://fcmwinsdrdxzizfngqig.supabase.co';
 const supabaseKey =
